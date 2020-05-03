af71ab8ea0f1655c4b52d5b32e478714066db4bc
Bohdan Shamborskyi <bshamborskyi@kaaiot.io>
2020-05-03 17:39:14 +0300

    Add QNX support

--- library/net_sockets.c
+++ library/net_sockets.c
@@ -47,7 +47,7 @@
 /* Enable definition of getaddrinfo() even when compiling with -std=c99. Must
  * be set before config.h, which pulls in glibc's features.h indirectly.
  * Harmless on other platforms. */
-#define _POSIX_C_SOURCE 200112L
+//#define _POSIX_C_SOURCE 200112L
 
 #if defined(__NetBSD__)
 #define _XOPEN_SOURCE 600 /* sockaddr_storage */
