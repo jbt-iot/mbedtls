726d95b0c9d9cff5ec40d47d12798aac3cbe01d2
Bohdan Shamborskyi <bshamborskyi@kaaiot.io>
2020-05-03 17:39:14 +0300

    Add QNX support

--- library/net_sockets.c
+++ library/net_sockets.c
@@ -22,7 +22,7 @@
 /* Enable definition of getaddrinfo() even when compiling with -std=c99. Must
  * be set before config.h, which pulls in glibc's features.h indirectly.
  * Harmless on other platforms. */
-#define _POSIX_C_SOURCE 200112L
+//#define _POSIX_C_SOURCE 200112L
 
 #if !defined(MBEDTLS_CONFIG_FILE)
 #include "mbedtls/config.h"
